8a887113064ca6dc39d302cea29dcda7eb804db9
manaskumar26 <67231881+manaskumar26@users.noreply.github.com>
2020-09-01 14:25:49 +0530

    Second Commit

--- assets/ajaxFormRequest.js
+++ assets/ajaxFormRequest.js
@@ -8,8 +8,6 @@
                 url:'/posts',
                 data:newPostForm.serialize(),
                 success:function(data){
-                    console.log(data.curuser)
-                    console.log(data.data.post)
                    let newPost=newPostDom(data.data.post,data.curuser);   
                    $('#feedContainer').prepend(newPost);  
                    newPostForm.value="";
@@ -24,16 +22,25 @@
     let newPostDom=(p,name)=>{
         return $(`<div class="flex-c">
             <div class="flexr">
-                <div><small>${name}</small></div>
+                <div><h5>${name}</h5></div>
                 <div><small>${ p.createdAt.toString().substring(0,10)}</small></div>
             </div>
             <div class="jumbotron flexr colorStyle">
-                ${ p.content }
+                ${p.content}
                     <div>
-                        <a href="/posts/delete/?id=${ p.id}"><i class="far fa-trash-alt"></i></a>
+                        <a href="/posts/delete/?id=${p._id}"><i class="far fa-trash-alt"></i></a>
                     </div>
             </div>
         </div>
+        <form action="/posts/create-comment" method="POST">
+            <div class="input-group input-group-sm mb-3">
+                <input type="text" class="form-control" placeholder="Type the Comment.." name="content">
+                <input type="hidden" class="form-control"  name="post" value="${p._id}">
+                <div class="input-group-append">
+                <button class="btn btn-success" type="submit">Comment</button>
+                </div>
+            </div>
+        </form>
         <div id="hrlength"></div>`)
     }
     createPost();
